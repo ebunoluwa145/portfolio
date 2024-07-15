b6dcbe20e452aeba5c89fc51293676c162e72307
ebunoluwa145 <ebunoluwaomosebi007@gmail.com>
2024-07-15 12:52:25 +0100

    added one more work

--- src/data.js
+++ src/data.js
@@ -6,61 +6,62 @@ import responsive from "../src/images/responsive.png"
 import login from "../src/images/login.png"
 import worldfood from "../src/images/worldfood.png"
 import firekicks from "../src/images/firekicks.png"
+import aventon from "../src/images/aventon.png"
 
 
 
-export const cardlist =[
-    {
-        id:1,
+export const cardlist = [{
+        id: 1,
         img: puzzles,
-        link:"#",
-        status:"Under construction"
+        link: "#",
+        status: "Under construction"
     },
     {
-        id:2,
+        id: 2,
         img: realEstate,
-        link:"https://home-choice.vercel.app/",
-        status:"Live"
+        link: "https://home-choice.vercel.app/",
+        status: "Live"
     },
     {
-        id:3,
-        img: chopfood,
-        link:"#",
-        status:"Webpage(static)"
-    },
-    {
-        id:4,
-        img: project,
-        link:"#",
-        status:"Webpage(static)"
-    },
-    {
-        id:5,
-        img: responsive,
-        link:"#",
-        status:"Webpage(static)"
-    },
-     {
-        id:6,
-        img: login,
-        link:"#",
-        status:"Webpage(static)"
-   },
-    {
-        id:7,
+        id: 3,
         img: worldfood,
-        link:"http://worldfood.lovestoblog.com/",
-        status:"live"
+        link: "http://worldfood.lovestoblog.com/",
+        status: "live"
     },
     {
-        id:8,
+        id: 4,
         img: firekicks,
-        link:"https://firekicks.lovestoblog.com/",
-        status:"live"
+        link: "https://firekicks.lovestoblog.com/",
+        status: "live"
+    },
+    {
+        id: 5,
+        img: chopfood,
+        link: "#",
+        status: "Webpage(static)"
+    },
+    {
+        id: 6,
+        img: project,
+        link: "#",
+        status: "Webpage(static)"
+    },
+    {
+        id: 7,
+        img: responsive,
+        link: "#",
+        status: "Webpage(static)"
+    },
+    {
+        id: 8,
+        img: login,
+        link: "#",
+        status: "Webpage(static)"
+    },
+
+    {
+        id: 9,
+        img: aventon,
+        link: "https://aventonconsulting.com.ng/"
     }
-//     {
-//         id:9,
-//         img: responsive,
-//         link:"#"
-//     }
- ]
+]
